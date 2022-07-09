87f57961474e4d4b254f05cbbc78026306063b6f
lamafab <42901763+lamafab@users.noreply.github.com>
2022-07-09 20:21:24 +0000

    generate cheatsheet for trigonometrical functions, update build script

--- level-4/computational-mathematics/student-notes/fabio-lama/build.sh
+++ level-4/computational-mathematics/student-notes/fabio-lama/build.sh
@@ -5,23 +5,28 @@
 gen_html() {
     # Remove suffix and prefix
     FILE=$1
-    OUT=${FILE#src/}
-    OUT=${OUT%.adoc}
+    OUT=${FILE%.adoc}
     HTML_OUT="cheatsheet_${OUT}.html"
 
     asciidoctor $FILE -o ${HTML_OUT}
 }
 
+# Change directory to src/ in order to have images included correctly.
+cd "$(dirname "$0")/src/"
+
 case $1 in
     html)
-        for FILE in src/*.adoc
+        for FILE in *.adoc
         do
             # Generate HTML file.
             gen_html ${FILE}
         done
+
+        # Move up from src/
+        mv *.html ../
         ;;
     png | img)
-        for FILE in src/*.adoc
+        for FILE in *.adoc
         do
             # Generate HTML file.
             gen_html ${FILE}
@@ -32,14 +37,17 @@ case $1 in
                 --enable-local-file-access \
                 --javascript-delay 2000\
                 $HTML_OUT $IMG_OUT
-
-            # Cleanup temporarily generated HTML files.
-            rm *.html > /dev/null 2>&1
         done
+
+        # Move up from src/
+        mv *.png ../
+
+        # Cleanup temporarily generated HTML files.
+        rm *.html > /dev/null 2>&1
         ;;
     clean)
-        rm *.html > /dev/null 2>&1
-        rm *.png > /dev/null 2>&1
+        rm ../*.html > /dev/null 2>&1
+        rm ../*.png > /dev/null 2>&1
         ;;
     *)
         echo "Unrecognized command"
